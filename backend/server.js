ac2b72fd1f524e6c81d1ebb5a680d57423bb17ad
kakrooakash <akash.kakroo.bergtech@gmail.com>
2025-02-03 13:02:33 +0530

    new update

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
+// Imported modules
 const express = require("express");
 const cors = require("cors");
 
+// Instance of express app
 const app = express();
 const PORT = 3500;
 
-app.use(cors());
-app.use(express.json());
+// Enable CORS to allow cross-origin requests
+app.use(cors()); 
 
+// Middleware to parse incoming requests with JSON payloads
+app.use(express.json()); 
+
+
+// Route for the root path | Test the working of backend server
 app.get("/", (req, res) => {
   res.send("Backend is running...");
 });
