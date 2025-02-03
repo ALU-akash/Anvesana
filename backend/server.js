918563bb5815808c2fdb7f1492706c190102d7d3
kakrooakash <kakrooakash74@gmail.com>
2025-02-03 13:09:23 +0530

    checking....

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,8 @@ app.get("/", (req, res) => {
   res.send("Backend is running...");
 });
 
+
+// Running server on PORT 3500
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
