3c0c69dd42bb6e8494103db16ec5093cb4b08e17
iamvanshkumar <vanshkumar.mail@gmail.com>
2025-02-05 11:14:32 +0530

    snake game added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import ResetPasswordPage from "./pages/ResetPasswordPage";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 import SignUpPage from "./pages/SignUpPage";
+import SnakeGamePage from "./pages/SnakeGamePage";
 import { UserProvider } from "./firebase/userContext";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path="/reset-password" element={<ResetPasswordPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/game" element={<SnakeGamePage />} />
       </Routes>
     </Router>
     </UserProvider>
