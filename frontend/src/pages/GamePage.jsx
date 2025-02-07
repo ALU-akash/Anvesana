57fe5146b5e17d6596077964b768de3c9019c367
iamvanshkumar <vanshkumar.mail@gmail.com>
2025-02-07 15:35:23 +0530

    fixed Game Page GUI UPDATED

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import SnakeBoard from "../components/SnakeBoard";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
-import { FaGamepad } from "react-icons/fa";
+import { FaDragon, FaGamepad } from "react-icons/fa";
 
 const GamePage = () => {
   return (
@@ -13,7 +13,6 @@ const GamePage = () => {
       {/* Main Content */}
       <main className="col-span-4 flex flex-col gap-4 h-full py-2 px-4 overflow-y-scroll">
         <Navbar />
-
         <div className="grid grid-cols-2 gap-4">
           <div className="bg_card rounded-lg shadow-sm p-4 flex flex-col gap-4">
             <h5 className="flex items-center gap-2 font-medium text-gray-600">
@@ -21,6 +20,13 @@ const GamePage = () => {
               Snake Game
             </h5>
             <SnakeBoard />
+          </div>
+          <div className="bg_card rounded-lg shadow-sm p-4 flex flex-col gap-4">
+            <h5 className="flex items-center gap-2 font-medium text-gray-600">
+              <FaDragon className="text-[#2BACDE]" />
+              Dino Game
+            </h5>
+            {/* <SnakeBoard /> */}
           </div>
         </div>
       </main>
