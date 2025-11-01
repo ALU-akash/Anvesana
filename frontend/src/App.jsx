506f4a7de20748aaa58488db543ee4c5e4de9742
iamvanshkumar <vanshkumar.mail@gmail.com>
2025-11-01 15:58:52 +0530

    dashboard started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import ResetPasswordPage from "./pages/ResetPasswordPage";
-import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 import SignUpPage from "./pages/SignUpPage";
 import GamePage from "./pages/gamePage";
@@ -12,10 +12,10 @@ function App() {
     <UserProvider>
     <Router>
       <Routes>
+        <Route path="/home" element={<HomePage />} />
         <Route path="/" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
         <Route path="/reset-password" element={<ResetPasswordPage />} />
-        <Route path="/home" element={<HomePage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/game" element={<GamePage />} />
       </Routes>
